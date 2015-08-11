00e14d82eab99025174578e3858a68b77ac7ea1a
Jessica Joly & Alyssa Horrocks <student@epicodus.com>
2015-08-11 09:37:15 -0700

    Pass second spec to set class as blank initially.

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -4,8 +4,15 @@ require('task')
 describe(Task)do
   describe("#description") do
     it("lets you give it a description") do
-    test_task = Task.new("scrub the zebra")
-    expect(test_task.description()).to(eq("scrub the zebra"))  
+      test_task = Task.new("scrub the zebra")
+      expect(test_task.description()).to(eq("scrub the zebra"))
     end
   end
+
+  describe(".all") do
+    it("leaves list empty at first") do
+      expect(Task.all()).to(eq([]))
+    end
+  end
+  
 end
